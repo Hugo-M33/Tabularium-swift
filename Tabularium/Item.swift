2f09bb2f4f4b80bf1687fa4806b44bc5e5def706
Hugo-M33 <hugo.martin.duloz+git@gmail.com>
2026-06-13 13:36:58 +0200

    Initial Commit

---
+++ Tabularium/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tabularium
+//
+//  Created by Hugo Martin on 13/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Tabularium/TabulariumApp.swift
@@ -0,0 +1,32 @@
+//
+//  TabulariumApp.swift
+//  Tabularium
+//
+//  Created by Hugo Martin on 13/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TabulariumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
